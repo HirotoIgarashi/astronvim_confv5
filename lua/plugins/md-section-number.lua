8733191536ab2c085b3dd98ad6661694831fe8e1
Hiroto Igarashi <hirotoigarashi@gmail.com>
2025-10-15 02:16:50 +0900

    add catppuccin.lua markdown-toc.lua md-section-numger.lua

---
+++ lua/plugins/md-section-number.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "whitestarrain/md-section-number.nvim",
+    ft = "markdown",
+    opts = {
+      max_level = 4, -- stop to add section number after max_level
+      min_level = 1, -- start to add section number after min_level
+      ignore_pairs = { -- the markdown content in these pairs will be ignored
+        { "```", "```" },
+        { "\\~\\~\\~", "\\~\\~\\~" },
+        { "<!--", "-->" },
+      },
+      toc = { -- toc sidebar config
+        width = 30,
+        position = "right",
+        indent_space_number = 2,
+        header_prefix = "- ",
+      },
+    },
+  },
+}
