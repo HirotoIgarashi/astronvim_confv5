b989cf25bcf6b70546e3b3eaf1364373aa886882
Hiroto Igarashi <hirotoigarashi@gmail.com>
2025-10-15 01:50:19 +0900

    update astrocore.lua

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -40,11 +40,58 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
+        -- カーソル行からの相対的な行番号を表示する
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        -- テキストの表示方法を変更します。バッファ内のテキストは変更されません。
+        -- trueにすると、ウィンドウの幅より長い行が折り返され、表示が次の行に 続きます。
+        -- falseの場合、行は折り返されず、長い行の一部のみが表示されます。
+        wrap = true, -- sets vim.opt.wrap
+        -- wrap = false, -- sets vim.opt.wrap
+
+        -- 以下は自分で設定
+        -- trueならタブキーを入力時にタブを数文字分のスペースに置き換える
+        expandtab = true,
+        -- タブ文字の幅を４文字分として表示する
+        tabstop = 4,
+        -- インデントのスペース幅を４文字とする
+        shiftwidth = 4,
+        -- smarttab = false,
+
+        -- カーソルが存在する行にハイライトを当ててくれます。
+        cursorline = true,
+
+        -- 80文字目にラインを入れる
+        colorcolumn = "80",
+
+        -- 挿入されるテキストの最大幅。長い行は、この幅を得るために空白の後に
+        -- 分割されます。値をゼロにすると、これが無効になります。
+        -- 「textwidth」がゼロの場合、「wrapmargin」が使用される可能性が
+        -- あります。
+        textwidth = 80,
+
+        -- formatoptions = "tcqj" <- default
+        -- t: 'textwidth'が設定されている場合、自動的にテキスト部分を改行する。
+        -- c: 自動的にコメントを改行。自動的にコメントリーダーを挿入する。
+        -- q: gqコマンドでコメントをフォーマットする。
+        --    Note that formatting will not change blank lines or lines
+        --    containing only the comment leader. A new paragraph starts after
+        --    such a line, or when the comment leader changes.
+        -- j: 複数行のコメントを連結する際に、余計なコメントリーダーを消す
+        -- r: 新しい行が挿入されるコメントにおけるコメントリーダーを挿入する。
+        --    automatically insert the current comment leader after hitting
+        --    <Enter> in Insert mode.
+        -- o: Oやoを使用して新しい行が作られるコメントにおいて、
+        --    コメントリーダーを挿入する。
+        --    automatically insert the current comment leader after hitting
+        --    'o' or 'O' in Normal mode. In case comment is unwanted in a
+        --    specific place use CTRL-U to quickly delete it. i_CTRL_U
+        -- local_vim.opt.formatoptions:append { m = true, M = true }
+        -- local_vim.opt.formatoptions = "tcqjrolmM"
+        -- formatoptions = "tcqjromM",
+        formatoptions = "tcqjn",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
