53df46e0c9ccb4288f40cac4d10584b57912ccee
HirotoIgarashi <hirotoigarashi@gmail.com>
2026-02-25 20:26:38 +0900

    add snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      -- Pickerのカスタマイズ設定
+    },
+    explorer = { enabled = true }, -- エクスプローラーを有効化
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    words = { enabled = true },
+    terminal = {
+      win = {
+        keys = {},
+      },
+    },
+    ---@class snacks.picker.db.Config
+    db = {
+      -- path to the sqlite3 library
+      -- If not set, it will try to load the library by name.
+      -- On Windows it will download the library from the internet.
+      -- sqlite3_path = "usr/bin/sqlite3",
+    },
+  },
+}
